a8d96ff667b52710669480749021f9a44fb05ae5
BLACKMOUNTAIN\Mike Laursen <michael.v.laursen@gmail.com>
2018-06-03 17:37:57 -0700

    OK, it gets kind of weird when you don't actually have anything you want the page to really do...

--- js/site.js
+++ js/site.js
@@ -1,12 +1,19 @@
 var myNamespace = (function() {
+    var _crazyCatWidth = 0;
+
     return {
         postInitialDelay: function() {
             $("#document-ready-indicator")
                 .removeClass("not-ready")
                 .addClass("ready");
+
+            $("#crazy-cat").attr("width", _crazyCatWidth);
+            _crazyCatWidth = 0;
         },
 
         createInitialDelay: function() {
+            _crazyCatWidth = $("#crazy-cat").width();
+            $("#crazy-cat").attr("width", 2 * _crazyCatWidth);
             setTimeout(this.postInitialDelay, 2000);
         }
     };
