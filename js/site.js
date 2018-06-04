5259dc09771ff3345304f74017c527d516dbeccf
BLACKMOUNTAIN\Mike Laursen <michael.v.laursen@gmail.com>
2018-06-04 10:48:30 -0700

    Do a slightly less silly cat animation.

--- js/site.js
+++ js/site.js
@@ -1,19 +1,27 @@
 var myNamespace = (function() {
-    var _crazyCatWidth = 0;
-
+    var _crazyCatCss = {
+        borderRadius: "100%",
+        opacity: "1.0",
+    }
+    
     return {
         postInitialDelay: function() {
             $("#document-ready-indicator")
                 .removeClass("not-ready")
                 .addClass("ready");
 
-            $("#crazy-cat").attr("width", _crazyCatWidth);
+            $("#crazy-cat")
+                .css("border-radius", _crazyCatCss.borderRadius)
+                .css("opacity", _crazyCatCss.opacity);
             _crazyCatWidth = 0;
         },
 
         createInitialDelay: function() {
-            _crazyCatWidth = $("#crazy-cat").width();
-            $("#crazy-cat").attr("width", 2 * _crazyCatWidth);
+            _crazyCatCss.borderRadius = $("#crazy-cat").css("border-radius");
+            _crazyCatCss.opacity = $("#crazy-cat").css("opacity");
+            $("#crazy-cat")
+                .css("border-radius", "50%")
+                .css("opacity", "0.5");
             setTimeout(this.postInitialDelay, 2000);
         }
     };
